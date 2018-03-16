e954cbd9d23a029b8c148c9169962f7146f53344
Илья Карев <ilya.karev1000@gmail.com>
2018-03-16 11:07:04 +0300

    deploy changes

--- app.js
+++ app.js
@@ -49,15 +49,15 @@ app.use(function(req, res, next) {
 });
 
 
-// app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 80);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist'));
 }
 
-// app.listen(app.get("port"), () => {
-//   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
-// });
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+});
 
 // error handler
 app.use(function(err, req, res, next) {
